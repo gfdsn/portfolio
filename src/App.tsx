90b8e607304d0b63f38ff62574c2d5b0da0223d2
Guilherme <guilhermefsantosn@gmail.com>
2024-07-28 21:59:26 +0100

    animations fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,36 @@ import styles from './styles/home.module.css';
 
 function App() {
 
-  const [animation, setAnimation] = useState('fade_up')
-  const welcomeRef = useRef<HTMLHeadingElement>(null);
-    
+  const [welcomeAnimation, setWelcomeAnimation] = useState('fade_up');
+  const [loginAnimation, setLoginAnimation] = useState('');
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  const animate = (fn: Function, duration: number) => {
+    return new Promise<void>((resolve) => {
+      setTimeout(() => {
+        fn();
+        resolve();
+      }, duration);
+    })
+  }
+  
   useEffect(() => { 
-    setTimeout(() => {
-      setAnimation('fade_down')
-    }, 5000);
+    
+    (async () => {
+      await animate(() => setWelcomeAnimation('fade_down'), 4000)
+      await animate(() => {
+        if(buttonRef.current) 
+          buttonRef.current.style.display = 'block';
+          setLoginAnimation('fade_up')
+      }, 2000)
+    })();
+    
   }, []);
 
   return (
     <div className={styles.welcome_animation}>
-      <h2 ref={welcomeRef} className={styles[animation]}>Welcome</h2>
+      <h2 className={styles[welcomeAnimation]}>Welcome</h2>
+      <button className={`${styles[loginAnimation]} ${styles.login_button}`} ref={buttonRef}>Login</button>
     </div>
   ); 
 }
